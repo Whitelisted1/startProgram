8497d3ed2662d55a2932fcf6e7ed0f306451b817
Whitelisted <77711834+Whitelisted1@users.noreply.github.com>
2022-05-04 21:36:27 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var spawn = require('child_process').spawn;
 
-const applicationPath = __dirname = "/test.js";
-console.log("Go into the code and modify the application path.")
+const applicationPath = __dirname + "/test.js";
 
 startApplication = spawn(applicationPath);
