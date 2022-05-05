616e5283375a059e9954d6dac0acce523785268f
Whitelisted <77711834+Whitelisted1@users.noreply.github.com>
2022-05-04 21:35:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 var spawn = require('child_process').spawn;
-var startApplication;
 
- // startApplication = spawn(__dirname + "/mac.command"); // MAC
- // startApplication = spawn(__dirname + "/windows.bat"); // WINDOWS
+const applicationPath = __dirname = "/test.js";
+console.log("Go into the code and modify the application path.")
 
- text = [
-   "Go into the code and uncomment the proper line depending on your operating system",
-   "Then go into the corresponding file (.bat for windows and .command for mac)",
-   "And replace 'path' with the application path"
- ]
-
- console.log(text)
+startApplication = spawn(applicationPath);
